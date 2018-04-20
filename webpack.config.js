295c1cab92286a95d258524a52375f6246ea98d3
pengteling <pengteling@gmail.com>
2018-04-20 18:25:36 +0800

    config

---
+++ webpack.config.js
@@ -0,0 +1,148 @@
+const webpack = require("webpack")
+const path = require("path")
+const isDev = process.env.NODE_ENV == "development"
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin")
+const config = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          extractCSS: isDev ? false : true
+        }
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(jpg|gif|png|jpeg|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-[sha512:hash:base64:7].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    })
+  ]
+}
+if (isDev) {
+  config.devtool = 'cheap-model-eval-source-map'
+  config.devServer = {
+    port: 8089,
+    host: '0.0.0.0',
+    hot: true,
+    historyApiFallback: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html'
+    })
+  )
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
+          }
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+
+    }
+  )
+}
+else {
+  /* 生产模式 */
+  config.entry = path.resolve(__dirname, 'src/Mplayer.vue')
+    //vendor: ['vue']
+  
+  config.output.filename = 'Mplayer.js'
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: ExtractTextWebpackPlugin.extract(
+        {
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                minimize: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      )
+
+
+    }
+  )
+  config.plugins.push(
+    new ExtractTextWebpackPlugin('style.[contentHash:8].css'),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor'
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'runtime'
+    // })
+  )
+
+
+}
+module.exports = config
