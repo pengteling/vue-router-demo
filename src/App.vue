10d4f932dc62af0f100110c3b905f2bfb3f94a30
pengteling <pengteling@gmail.com>
2018-04-20 20:31:06 +0800

    router-demo1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <Mplayer
-    url="http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
-    :autoplay="true"
-    :paused="false"
-    volume=80    
-  />
+  <div>
+    <h2>App</h2>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-import Mplayer from 'vue-simple-mplayer2'
+
 export default{
-  components:{
-    Mplayer
-  }
+  
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Router from 'vue-router'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+const router = new Router({
+  mode:'history',
+  routes
+})
 // const root = document.createElement('div')
 // document.body.appendChild(root)
 //k+1
+/* 
+route 单条路由
+routes 一组路由
+router 路由管理者
+客户端路由实现方式： 
+mode: hash  (#)
+      history 
+ */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
